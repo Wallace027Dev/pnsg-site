9fecac00799c881838a293866b21c82d35a86f2f
Wallace Vieira <wallaceofc@hotmail.com>
2024-01-04 09:58:50 -0300

    header ok

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,76 @@
+import { Content, Header, SideBar } from './styles';
+
+import menu from '../../assets/images/menu.svg';
+import logo from '../../assets/images/logo.svg';
+import instagram from '../../assets/images/instagram.svg';
+
 function App() {
+  function handleMenuClick() {
+    const sidebar = document.getElementById('sidebar');
+    const content = document.getElementById('content');
+    sidebar.style.width = '250px';
+    content.classList.add('blur');
+  }
+
+  function handleLinkClick() {
+    const sidebar = document.getElementById('sidebar');
+    const content = document.getElementById('content');
+    sidebar.style.width = '0';
+    content.classList.remove('blur');
+  }
+
   return (
-    <div>
-      <p>Hello World</p>
-    </div>
+    <>
+      <Header>
+        <button id="menuBtn" onClick={handleMenuClick}>
+          <img src={menu} alt="Menu" />
+          <span>MENU</span>
+        </button>
+        <img
+          className="header-logo"
+          src={logo}
+          alt="Paróquia Nossa Senhora Da Glória"
+        />
+        <div className="header-search">
+          <img src={instagram} alt="Instagram" />
+        </div>
+      </Header>
+
+      <SideBar id="sidebar">
+        <div className="nav-header">
+          <img src={logo} alt="Paróquia Nossa Senhora da Glória Horizontal" />
+          <h1>Paróquia Nossa Senhora da Glória</h1>
+          <p>"Nossa senhora da Glória, rogai por nós"</p>
+        </div>
+        <a href="#section1" onClick={handleLinkClick}>
+          Section 1
+        </a>
+        <a href="#section2" onClick={handleLinkClick}>
+          Section 2
+        </a>
+        <a href="#section3" onClick={handleLinkClick}>
+          Section 3
+        </a>
+        <button className="close-btn" onClick={handleLinkClick}>
+          X
+        </button>
+      </SideBar>
+
+      <Content id="content">
+        <section id="section1">
+          <h2>Section 1</h2>
+          <p>Conteúdo da Seção 1</p>
+        </section>
+        <section id="section2">
+          <h2>Section 2</h2>
+          <p>Conteúdo da Seção 2</p>
+        </section>
+        <section id="section3">
+          <h2>Section 3</h2>
+          <p>Conteúdo da Seção 3</p>
+        </section>
+      </Content>
+    </>
   );
 }
 
---
+++ src/components/App/styles.js
@@ -0,0 +1,107 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  background: var(--primary);
+  height: 55px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 100px;
+
+  button {
+    display: flex;
+    align-items: center;
+    border: none;
+    background: transparent;
+
+    img {
+      height: 25px;
+      margin-right: 12px;
+    }
+
+    span {
+      font-size: 13px;
+      font-weight: 700;
+    }
+  }
+
+  .header-logo {
+    height: 30px;
+    position: absolute;
+    left: 50%;
+  }
+
+  .header-search {
+    img {
+      height: 28px;
+    }
+  }
+`;
+
+export const SideBar = styled.div`
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background: var(--primary);
+  overflow-x: hidden;
+  transition: 0.5s;
+  padding-top: 60px;
+  box-shadow: 2px 0 5px primary;
+
+  a {
+    padding: 15px 10px;
+    text-decoration: none;
+    font-size: 18px;
+    color: var(--tertiary);
+    display: block;
+    transition: 0.3s;
+  }
+
+  a:hover {
+    color: var(--bg);
+  }
+
+  a:focus {
+    outline: none;
+  }
+
+  .close-btn {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    font-size: 20px;
+    background: var(--tertiary);
+    color: var(--primary);
+    padding: 2px 6px;
+    border-radius: 50%;
+    border: none;
+    cursor: pointer;
+  }
+
+  .nav-header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    color: var(--tertiary);
+
+    img {
+      height: 74px;
+      margin-bottom: 12px;
+    }
+
+    h1,
+    p {
+      font-size: 12px;
+    }
+  }
+`;
+
+export const Content = styled.div`
+  margin-left: 0;
+  transition: margin-left 0.5s;
+  padding: 16px;
+`;
