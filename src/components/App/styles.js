00d892aad71855b82acc68eddd97ccc47ba9b2e5
Wallace Vieira <wallaceofc@hotmail.com>
2024-01-10 10:36:16 -0300

    feat: books indication; films indication

--- src/components/App/styles.js
+++ src/components/App/styles.js
@@ -287,7 +287,7 @@ export const HomiliaVideo = styled.div`
   }
 `;
 
-export const SlideBooks = styled.div`
+export const SlideIndicationsContainer = styled.div`
   margin: 48px auto;
   text-align: center;
 
@@ -295,11 +295,40 @@ export const SlideBooks = styled.div`
     margin: 10px 0 30px 0;
   }
 
-  a {
-    img {
-      width: 350px;
-      height: 200px;
-      object-fit: cover;
+  li {
+    position: relative;
+
+    a {
+      overflow: hidden;
+
+      img {
+        height: 250px;
+        object-fit: cover;
+      }
+
+      h1 {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        font-size: 16px;
+        opacity: 0;
+        text-decoration: none;
+        color: var(--bg);
+        max-width: 100px;
+      }
+    }
+    :hover {
+      cursor: pointer;
+
+      img {
+        filter: brightness(35%);
+      }
+
+      h1 {
+        opacity: 1;
+        transition: opacity 1.25s ease;
+      }
     }
   }
 `;
